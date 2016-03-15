ad94813a6e5df9ba08999c880078f3b4d036e553
MrxBlackWolf <nikola.milicevic.169.12@ict.edu.rs>
2016-03-15 22:49:57 +0100

    Meniiii tebiiiiiiiiiiiiiiiiiii

--- application/views/meni.php
+++ application/views/meni.php
@@ -1,17 +1,23 @@
 <div id="nav" >
 	<ul> 	
-             <?php foreach ($meni as $link){ ?>
-            <?php if($link['naziv']!='Likovi') { ?>
-                <li> <a href="<?php echo $base_url.$link['link']; ?>"><?php echo $link['naziv'] ?></a></li>
-            <?php }?>
-                <?php if($link['podmeni']==1) { ?>
-                    <li> <a title="<?php  echo $base_url.$link['naziv'] ?>" style="cursor:pointer;" id="lop"><?php  echo $link['naziv']; ?></a>
-			<ul id="drop">	
-                            <?php foreach ($podmeni as $podlink){ ?>			
-                                <li><a href="<?php echo $base_url; ?>lik/vrsta/<?php echo $podlink['id_lika']?>" title="<?php echo $podlink['podmeni_naziv'] ?>" > <?php echo $podlink['podmeni_naziv'] ?> </a></li>
-                            <?php }  ?>
-			</ul>
-                    </li>
-             <?php } }?> 	
+		<?php foreach ($meni as $link) { ?>
+			<?php if ($link['naziv'] != 'Likovi' && $link['naziv'] != 'Dokumentacija') { ?>
+				<li> <a href="<?php echo $base_url . $link['link']; ?>"><?php echo $link['naziv'] ?></a></li>
+			<?php } ?>
+
+			<?php if ($link['naziv'] == 'Dokumentacija') { ?>
+				<li> <a href="<?php echo $base_url . $link['link']; ?>" target="_blank"><?php echo $link['naziv'] ?></a></li>
+			<?php } ?>
+
+			<?php if ($link['podmeni'] == 1) { ?>
+				<li> <a title="<?php echo $base_url . $link['naziv'] ?>" style="cursor:pointer;" id="lop"><?php echo $link['naziv']; ?></a>
+					<ul id="drop">	
+						<?php foreach ($podmeni as $podlink) { ?>			
+							<li><a href="<?php echo $base_url; ?>lik/vrsta/<?php echo $podlink['id_lika'] ?>" title="<?php echo $podlink['podmeni_naziv'] ?>" > <?php echo $podlink['podmeni_naziv'] ?> </a></li>
+						<?php } ?>
+					</ul>
+				</li>
+			<?php }
+		} ?> 	
 	</ul>
 </div>
